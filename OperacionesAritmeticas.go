5ba523ce6bdffffe66bc332e138c6d96c4c1f206
jrorozco <rene@alvaroquintana.com>
2018-04-10 19:10:06 -0500

    Operaciones aritmeticas

---
+++ OperacionesAritmeticas.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero1 int = 10
+	var numero2 int = 6
+
+	//suma
+	fmt.Print("La suma es ")
+	fmt.Println(numero1 + numero2)
+
+	//resta
+	fmt.Print("La resta es ")
+	fmt.Println(numero1 - numero2)
+
+	//multiplicacion
+	fmt.Print("La multiplicacion  es ")
+	fmt.Println(numero1 * numero2)
+
+	//division
+	fmt.Print("La division  es ")
+	fmt.Println(numero1 / numero2)
+
+	//Sacar el resto de la division
+	fmt.Print("El resto de la division  es ")
+	fmt.Println(numero1 % numero2)
+
+}
